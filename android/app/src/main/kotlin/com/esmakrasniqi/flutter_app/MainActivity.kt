c1ba773961ecdf63b6da0dc8ec07f0f33db2c402
Esma Krasniqi <krasniqiesma@gmail.com>
2023-01-20 19:24:30 +0100

    Init commit - Hands on flutter

---
+++ android/app/src/main/kotlin/com/esmakrasniqi/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.esmakrasniqi.flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.green[400],
+          title: Text("Hello World"),
+        ),
+        body: Center(
+          child: Column(
+            children: const [
+              Text(
+                "Hello World!",
+                style: TextStyle(
+                  fontFamily: 'Kaushan',
+                  fontSize: 28.0,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 150,
+              ),
+              Image(
+                image: NetworkImage(
+                    'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80'),
+                height: 500,
+                width: 250,
+              ),
+            ],
+          ),
+        ),
+        backgroundColor: Colors.grey,
+      ),
+    ),
+  );
+}
+
+// SafeArea(
+// child: Center(
+// child: Text('Hello World'),
+// ),
+// ),
