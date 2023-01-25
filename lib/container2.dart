999585891f430829f89bef750167fc0a465c1f81
Esma Krasniqi <krasniqiesma@gmail.com>
2023-01-25 18:22:15 +0100

    Card/Column

--- lib/container2.dart
+++ lib/container2.dart
@@ -29,42 +29,80 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text('Menu'),
-          centerTitle: true,
-        ),
-        body: Container(
-          // color: Colors.white,
-          child: Row(
-            children: const [
-              Image(
-                image: NetworkImage(
-                    'https://cdn-icons-png.flaticon.com/512/2927/2927347.png'),
-                height: 50,
+          backgroundColor: Colors.grey,
+          appBar: AppBar(
+            title: Text('Menu'),
+            centerTitle: true,
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
                 width: 50,
+                height: 50,
+                color: Colors.red,
               ),
-              SizedBox(
-                width: 25,
+              Container(
+                width: 75,
+                height: 50,
+                color: Colors.blue,
               ),
-              Text(
-                "Pizza",
-                style: TextStyle(
-                  fontSize: 28.0,
-                  color: Colors.white,
-                ),
+              Container(
+                width: 100,
+                height: 50,
+                color: Colors.green,
+              ),
+              Container(
+                width: double.infinity, //make container full screen
               ),
             ],
-          ),
-          padding: EdgeInsets.all(15.0),
-          margin: EdgeInsets.all(15.0),
-          decoration: BoxDecoration(
-            color: Colors.orange,
-            border: Border.all(width: 5.0, color: Colors.transparent),
-            borderRadius: BorderRadius.circular(15.0),
-          ),
-        ),
-      ),
+          )),
     );
   }
+}
+
+Card getCard() {
+  return Card(
+    child: ListTile(
+      leading: Icon(Icons.add),
+      title: Text('Title'),
+      subtitle: Text('subtitle'),
+      trailing: Icon(Icons.remove),
+    ),
+    margin: EdgeInsets.all(10.0),
+  );
+}
+
+Container getContainer() {
+  return Container(
+    // color: Colors.white,
+    child: Row(
+      children: const [
+        Image(
+          image: NetworkImage(
+              'https://cdn-icons-png.flaticon.com/512/2927/2927347.png'),
+          height: 50,
+          width: 50,
+        ),
+        SizedBox(
+          width: 25,
+        ),
+        Text(
+          "Pizza",
+          style: TextStyle(
+            fontSize: 28.0,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    ),
+    padding: EdgeInsets.all(15.0),
+    margin: EdgeInsets.all(15.0),
+    decoration: BoxDecoration(
+      color: Colors.orange,
+      border: Border.all(width: 5.0, color: Colors.transparent),
+      borderRadius: BorderRadius.circular(15.0),
+    ),
+  );
 }
