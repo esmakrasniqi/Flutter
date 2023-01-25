569c01c03e8716969e333ca8648c7b2c22566ad4
Esma Krasniqi <krasniqiesma@gmail.com>
2023-01-25 19:16:43 +0100

    Circled image using Widget/Container

--- lib/main.dart
+++ lib/main.dart
@@ -12,25 +12,39 @@ void main() {
         ),
         body: Center(
           child: Column(
-            children: const [
-              Text(
-                "Hello World!",
-                style: TextStyle(
-                  fontFamily: 'Kaushan',
-                  fontSize: 28.0,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 25,
-              ),
+            children: [
               Image(
                 //   image: NetworkImage(
                 // 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80'),
                 image: AssetImage('images/img.jpg'),
-                height: 500,
+                height: 250,
                 width: 250,
+              ),
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80'),
+                radius: 75.0,
+                backgroundColor: Colors.transparent,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              GestureDetector(
+                onTap: () {
+                  print('Hello World');
+                },
+                child: Container(
+                  clipBehavior: Clip.hardEdge,
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(75.0),
+                  ),
+                  child: Image(
+                    image: AssetImage('images/img.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
             ],
           ),
